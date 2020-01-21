e5798ce51481a6ccf6a7ba7548e50b30747c790f
Sudesh <Sudesh@SudeshSunichura>
2020-01-21 13:01:30 -0500

    RoundBead Class

---
+++ src/beads/RoundBead.java
@@ -0,0 +1,41 @@
+/*
+ * Sudesh Sunichura
+ * Student ID: 991421780
+ * SYST10199 - Web Programming
+ */
+package beads;
+
+/** This class represents round bead
+ *
+ * @author Sudesh
+ */
+public class RoundBead {
+    String colour;
+    int radius;
+    char letter;
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+    
+}
