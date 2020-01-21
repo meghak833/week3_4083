846c2a63e5a75fe3d77913d466ff3528d9f96a0d
Steven <Steven.Michalec18@gmail.com>
2020-01-21 13:07:33 -0500

    Added RoundBead.java to project.

---
+++ src/beads/RoundBead.java
@@ -0,0 +1,37 @@
+
+/*
+@author Steven Michalec
+*/
+public class RoundBead{
+    String color;
+    int radius;
+    char letter;
+    
+    /** getter for color
+     * @return the color
+     */
+        public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the letter
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * @param letter the letter to set
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+}
