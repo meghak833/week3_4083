1f8d2f7b09004942bca22c7d912a1a6fa83cff04
LAPTOP <LAPTOP@10.48.8.201>
2020-01-21 13:01:37 -0500

    RoundBead nidek class added

---
+++ src/beads/RoundBead.java
@@ -0,0 +1,49 @@
+package beads;
+
+public class RoundBead {
+    private String color;
+    private int radius;
+    private char letter;
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * @return the letter
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * @param letter the letter to set
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+}
