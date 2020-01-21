52e3141c7ec961a219d52f02cfbc2026092e0274
Mukti Patel <patmukti@sheridancollege.ca>
2020-01-21 13:02:06 -0500

    RoundBead created

---
+++ src/beads/RoundBead.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beads;
+
+/**
+ *
+ * @author mukti
+ */
+public class RoundBead {
+    private String color;
+    private int radius;
+    private char letters;
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * @return the letters
+     */
+    public char getLetters() {
+        return letters;
+    }
+
+    /**
+     * @param letters the letters to set
+     */
+    public void setLetters(char letters) {
+        this.letters = letters;
+    }
+    
+}
