f04d56e01a48669d1bc2b93d98e6aa85f1c0f5b8
megha <megha.patel2@sheridancollege.ca>
2020-01-21 13:01:18 -0500

    RoundBead model class added

---
+++ src/beads/RoundBead.java
@@ -0,0 +1,56 @@
+
+package beads;
+
+/** This class represents round beads
+ *
+ * @author Megha Patel
+ */
+public class RoundBead {
+    
+    private String color;
+    private int radius;
+    private char letter;
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * @return the letter
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * @param letter the letter to set
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+
+}
