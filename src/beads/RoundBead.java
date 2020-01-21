948778e4787c19e55dc4b3909fb9b51e5e06fb34
User <User@DESKTOP-4MFQ21B>
2020-01-21 13:03:58 -0500

    first Commitment of StudentList

---
+++ src/beads/RoundBead.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beads;
+
+/**
+ * 
+ * @author User
+ */
+public class RoundBead {
+    
+    private String color;
+    private int radius;
+    private char letter;
+    
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * @return the letter
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * @param letter the letter to set
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+ 
+    
+}
